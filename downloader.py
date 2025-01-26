f55c82bf6fd0ab955bb2237630c01f104575ef9f
Ren Chen <xuanrenchen7@gmail.com>
2025-01-26 04:57:41 -0500

    refactor: added downloader class

--- downloader.py
+++ downloader.py
@@ -44,7 +44,6 @@ class Song:
 
 class Downloader:
     def get_url(self, content):
-        regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
         regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),#]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
         if re.search(regex, content):
